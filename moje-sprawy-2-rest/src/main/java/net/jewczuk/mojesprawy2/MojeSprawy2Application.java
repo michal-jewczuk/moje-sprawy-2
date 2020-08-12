7491ab7701d65141b95af56d80a283dbac0b5305
Michal Jewczuk <michal.jewczuk@outlook.com>
2020-08-12 14:46:30 +0200

    Add basic modules: domain and rest

---
+++ moje-sprawy-2-rest/src/main/java/net/jewczuk/mojesprawy2/MojeSprawy2Application.java
@@ -0,0 +1,12 @@
+package net.jewczuk.mojesprawy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojeSprawy2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MojeSprawy2Application.class, args);
+    }
+}
---
+++ moje-sprawy-2-rest/src/test/java/net/jewczuk/mojesprawy2/MojeSprawy2ApplicationTests.java
@@ -0,0 +1,12 @@
+package net.jewczuk.mojesprawy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MojeSprawy2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
