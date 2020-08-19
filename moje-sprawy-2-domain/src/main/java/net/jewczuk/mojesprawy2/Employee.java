54c86e787e695308b6163839962902e85eecc793
Michal Jewczuk <michal.jewczuk@outlook.com>
2020-08-19 15:15:22 +0200

    Create employee entity

---
+++ moje-sprawy-2-domain/src/main/java/net/jewczuk/mojesprawy2/Employee.java
@@ -0,0 +1,44 @@
+package net.jewczuk.mojesprawy2;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee extends AbstractEntity {
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "login", nullable = false, unique = true)
+    private String login;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "active", nullable = false)
+    private boolean active;
+
+    @Column(name = "include", nullable = false)
+    private boolean include;
+
+    @Column(name = "accessLevel", nullable = false)
+    private int accessLevel;
+
+}
